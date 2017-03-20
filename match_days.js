c566b63187f5799b9dcdc2c6a9e2119dd4896a09
rendaniluk <lukhwa11582924>
2017-03-20 17:07:15 +0200

    matching_days web-page

--- match_days.js
+++ match_days.js
@@ -4,7 +4,7 @@ var secondDay = document.querySelector('.dy2');
 var matchDaysBtn = document.querySelector('.theButton');
 var list = document.querySelectorAll('.day')
 //#########################The Dom END############################
-var getDays = function(date) {            //getDay function
+var getDayIndex = function(date) {            //getDay function
     var specificDay = date.getDay();
     return specificDay;
 };
@@ -12,11 +12,11 @@ matchDaysBtn.addEventListener('click', () => { //eventLestiner to Match Day Btn
     clear();    //calling a clear function to clear previuos matched day
     var that_day = new Date(firstDay.value);  //variable for date input
     var that_day1 = new Date(secondDay.value);
-    if (getDays(that_day) === getDays(that_day1)) { //conditional to check if days match
-        list[getDays(that_day)].classList.add('matched');
+    if (getDayIndex(that_day) === getDayIndex(that_day1)) { //conditional to check if days match
+        list[getDayIndex(that_day)].classList.add('matched');
     } else {
-        list[getDays(that_day)].classList.add('day0');
-        list[getDays(that_day1)].classList.add('day1');
+        list[getDayIndex(that_day)].classList.add('day0');
+        list[getDayIndex(that_day1)].classList.add('day1');
     }
 });
 var clear = function() {        //function to clear previuos matched day
